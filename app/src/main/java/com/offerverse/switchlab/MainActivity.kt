d986f213a67a6a83437332436fbb452a046bed18
Pritesh Jain <123294193+priteshjain28@users.noreply.github.com>
2026-07-01 18:06:11 -0700

    Add minimal MainActivity

--- app/src/main/java/com/offerverse/switchlab/MainActivity.kt
+++ app/src/main/java/com/offerverse/switchlab/MainActivity.kt
@@ -1,1 +1,13 @@
-x
+package com.offerverse.switchlab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { Text("SwitchLab") }
+    }
+}
